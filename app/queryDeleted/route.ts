f77b823379fe86ce2b29df660b9599a02a18fe2b
KRISHAN NANDAN GOSWAMI <krishannandang@crldomain.com>
2024-12-27 11:47:47 +0530

    Ch 7 request Waterfall Issue

--- app/queryDeleted/route.ts
+++ app/queryDeleted/route.ts
@@ -2,12 +2,26 @@ import { db } from "@vercel/postgres";
 
 const client = await db.connect();
 
-async function listInvoices() {
+// async function listInvoices() {
+//   const data = await client.sql`
+//     SELECT invoices.amount, customers.name
+//     FROM invoices
+//     JOIN customers ON invoices.customer_id = customers.id
+//     WHERE invoices.amount = 666;
+//   `;
+
+//   return data.rows;
+// }
+async function listRevenueTable() {
   const data = await client.sql`
-    SELECT invoices.amount, customers.name
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
+    SELECT * FROM revenue
+  `;
+
+  return data.rows;
+}
+async function listInvoicesTable() {
+  const data = await client.sql`
+    SELECT * FROM invoices
   `;
 
   return data.rows;
@@ -19,7 +33,8 @@ export async function GET() {
   //     "Uncomment this file and remove this line. You can delete this file when you are finished.",
   // });
   try {
-    return Response.json(await listInvoices());
+    // return Response.json(await listRevenueTable());
+    return Response.json(await listInvoicesTable());
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
